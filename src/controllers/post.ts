018fdac7d07c008ca15786a06894ef00155581e4
Solex <solomonyunana95@gmail.com>
2021-12-01 07:19:06 +0100

    fixed object id issue

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -32,7 +32,7 @@ export async function CreatePost(req: Request, res: Response) {
       return ProcessingSuccess(res, post);
     }
     const updatePost = await models.Post.updateOne(
-      { postId: new Types.ObjectId(postId) },
+      { postId: Types.ObjectId(postId) },
       {
         coverImage,
         price,
